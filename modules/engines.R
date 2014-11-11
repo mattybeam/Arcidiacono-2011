88ce651be5b5142f257cba67fbfd6efb161b0c0c
Matt Beamer <matthewbeamer2018@u.northwestern.edu>
2014-11-11 10:23:00 -0600

    Upload Simulates Data on 1 million engines

--- modules/engines.R
+++ modules/engines.R
@@ -3,6 +3,8 @@
 #   Column 1: engine number, from 1 to N
 #   Column 2: type, 1 for s_1 and 2 for s_2
 #   Column 3: mileage at replacement, x
+#
+# Save using saveRDS() for later use
 
 todeath <- function(type){
   laply(1:x_m,function(l) rbinom(1,1,p[l,type])) %>%
@@ -11,10 +13,12 @@ todeath <- function(type){
 
 engines <- data.frame(
   engine_number = 1:N,
-  type = rbinom(N,1,1 - pi_s),
-  x0 <- rep(0,N)
+  type = rbinom(N,1,1 - pi_s)
   ) %>%
   mutate(type = type + 1)
 
+
 engines$x <- apply(engines,1,function(y) todeath(y[2]))
 engines <- mutate(engines, x = x - 1)
+
+saveRDS(engines,"engines_data.rds")
