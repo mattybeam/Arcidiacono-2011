8f9faaa46fd95caffcdd9dea6986e0a6f149de28
CY-Chan <j-chan@kellogg.northwestern.edu>
2014-11-19 11:10:28 -0600

    Added result comparison table.
    Housecleaning and commenting.

--- code2/modules/estimatePrimitives.R
+++ code2/modules/estimatePrimitives.R
@@ -6,7 +6,8 @@
 #        max.iter, convergence criterion, stops EM algorithm if it gets stuck (Default = 50)
 # OUTPUT: thetahat, estimated parameters theta and pi_s
 
-estimatePrimitives <- function(s.val,beta,est.error = 10^-3,max.iter = 50){
+estimatePrimitives <- function(s.val, beta, est.error = 10^-3, max.iter = 50){
+  # Randomize starting initial values
   p <- runif(1,min = 0.1,max = 1)
   pi_s <- c(p,1-p)
   theta <- c(runif(1,min = 6,max = 14),runif(1,min = 0.1,max = 2))
@@ -16,16 +17,25 @@ estimatePrimitives <- function(s.val,beta,est.error = 10^-3,max.iter = 50){
   est <- list(CCP = CCP,pi_s = pi_s, theta = theta)
   delta <- est.error + 1
   iter <- 1
+  
+  # Run EM operator until convergence or max iterations reached
   while(delta > est.error & iter < 50){
-    print("est")
     est.next <- try(EM.operator(CCP=est$CCP, est$pi_s, est$theta),silent = TRUE)
+    
+    # Check if bad initial values. End EM operator and return -Inf if true.
     if ('try-error' %in% class(est.next)){
       est$MLEval = -Inf
       break
     } 
-    else delta=max(abs(est$CCP - est.next$CCP),abs(est$pi_s - est.next$pi_s),abs(est$theta - est.next$theta))
+    else {
+      delta=max(abs(est$CCP - est.next$CCP),
+                abs(est$pi_s - est.next$pi_s),
+                abs(est$theta - est.next$theta))
+    }
+    
     est=est.next
     iter <- iter + 1
   }
+  
   return(est)
 }
